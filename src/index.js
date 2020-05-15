dfc4cbe2d955d17b6cf4b746d2880c92de1ec0ce
Alesya <alesya.zarembo@mail.ru>
2020-05-16 01:00:51 +0300

    feat: implementation swiper

--- src/index.js
+++ src/index.js
@@ -7,3 +7,41 @@ import getTranslation from './models/MovieTranslate';
 import './style.css';
 
 Swiper.use([Navigation, Pagination, Scrollbar]);
+
+const mySwiper = new Swiper('.swiper-container', {
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    // when window width is >= 1020px
+    1020: {
+      slidesPerView: 4,
+      spaceBetween: 100,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const next = document.querySelector('.swiper-button-next');
+next.onclick = () => { mySwiper.slideNext(); };
+const prev = document.querySelector('.swiper-button-prev');
+prev.onclick = () => { mySwiper.slidePrev(); };
