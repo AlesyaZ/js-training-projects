848927797e654cbe8220dec96d594e5fcd85fe66
Alesya <alesya.zarembo@mail.ru>
2020-05-16 01:03:46 +0300

    feat: creat cards movie

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import './style.css';
 
 Swiper.use([Navigation, Pagination, Scrollbar]);
 
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+
 const mySwiper = new Swiper('.swiper-container', {
   breakpoints: {
     // when window width is >= 320px
@@ -40,6 +42,39 @@ const mySwiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+function CreateMovieCard(title, poster, year, imdbID, rate) {
+  const cards = document.createElement('div');
+  cards.classList.add('swiper-slide');
+
+  const titleElem = document.createElement('p');
+  titleElem.classList.add('card-title');
+  titleElem.innerHTML = title;
+
+  const titleLink = document.createElement('a');
+  titleLink.href = `https://www.imdb.com/title/${imdbID}/videogallery/`;
+  titleLink.appendChild(titleElem);
+
+  const posterELem = document.createElement('div');
+  posterELem.classList.add('poster');
+  posterELem.innerHTML = `<img src = ${poster === 'N/A' ? '../src/images/not_img.png' : poster} onerror="this.src='../src/images/not_img.png'">`;
+
+  const yearElem = document.createElement('p');
+  yearElem.classList.add('movie-year');
+  yearElem.innerHTML = year;
+
+  const rateElem = document.createElement('span');
+  rateElem.classList.add('rate');
+  rateElem.innerHTML = `<img class='star' src='../src/images/star.png'> ${rate}`;
+
+  cards.appendChild(titleLink);
+  cards.appendChild(posterELem);
+  cards.appendChild(yearElem);
+  cards.appendChild(rateElem);
+  swiperWrapper.appendChild(cards);
+  mySwiper.appendSlide(cards);
+}
+CreateMovieCard();
 
 const next = document.querySelector('.swiper-button-next');
 next.onclick = () => { mySwiper.slideNext(); };
