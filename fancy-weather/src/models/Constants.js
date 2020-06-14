1438834159c6eb4671672dfff65df6531017634a
Alesya <alesya.zarembo@mail.ru>
2020-06-14 23:17:46 +0300

    feat: add models constants-Key

---
+++ fancy-weather/src/models/Constants.js
@@ -0,0 +1,8 @@
+const TokenGeolocation = '968ceea5bad489';
+export const KeyBackground = 'USC3LD1cqqrmazS3D0-tOdbZ3uJCnY8mp2dNCr3jx_M';
+export const TokenMaps = 'pk.eyJ1IjoiYWxlc3lheiIsImEiOiJja2FzcXV3NXgwYW4yMnhvMzg4czZwNHppIn0.eRktgzJEw8kByDotScdMVg';
+export const KeyGeocoding = '63350573c8984c5bb406bf732af294fb';
+export const KeyTranslate = 'trnsl.1.1.20200501T111237Z.5a0d582363b62eb5.75a35eae2bcb27a2db5d5e37cdff02cebeb24a65';
+export const KeyWeater = '85b0b47f49c34c76964bffe1c241bc6f';
+export const GeolocationUrl = `https://ipinfo.io/json?token=${TokenGeolocation}`;
+export const StyleMap = 'mapbox://styles/mapbox/streets-v11';
